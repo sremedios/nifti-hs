510b28a1dbb35087ea79aa8a8e33ae391f94e622
sremedios <sam.remedios@gmail.com>
2019-11-07 15:48:36 -0600

    formatting struct

--- src/nifti_header.hs
+++ src/nifti_header.hs
@@ -1,77 +1,110 @@
-data NiftiHeader = NiftiHeader{ 
-    sizeof_hdr :: Data.Word32
-}
+-- array types used in header
+type Dim = (Data.Word8, Data.Word8, Data.Word8, Data.Word8, Data.Word8, Data.Word8, Data.Word8, Data.Word8,)
+type PixDim = (Float, Float, Float, Float, Float, Float, Float, Float,)
 
-{-
-NIFTI header specification from https://nifti.nimh.nih.gov/pub/dist/src/niftilib/nifti1.h
+-- header itself
+data Nifti1Header = Nifti1Header
+    { sizeof_hdr        :: Data.Word32      -- MUST be 348
+    , dim_info          :: Data.Word8       -- MRI slice ordering
+    , dim               :: Dim              -- Data array dimensions
+    , intent_p1         :: Float            -- 1st intent parameter
+    , intent_p2         :: Float            -- 2nd intent parameter
+    , intent_p3         :: Float            -- 3rd intent parameter
+    , intent_code       :: Data.Word8       -- NIFTIINTENT code
+    , data_type         :: Data.Word8       -- Defines data type 
+    , bitpix            :: Data.Word8       -- number of bits/voxel
+    , slice_start       :: Data.Word8       -- first slice index
+    , pixdim            :: PixDim           -- grid spacing
+    , vox_offset        :: Float            -- offset into .nii file
+    , scl_slope         :: Float            -- Data scaling: slope
+    , scl_inter         :: Float            -- data scaling: offset
+    , slice_end         :: Data.Word8       -- Last slice index
+    , slice_code        :: Data.Word8       -- Slice timing order
+    , xyzt_units        :: Data.Word8       -- Units of pixdim[1..4]
+    , cal_max           :: Float            -- Max display intensity
+    , cal_min           :: Float            -- Min display intensity
+    , slice_duration    :: Float            -- Time for 1 slice
+    , toffset           :: Float            -- Time axis shift
+    , descrip           :: String           -- Any text description, total size is 80 bytes
+    , aux_file          :: String           -- auxiliary filename, total size is 24 bytes
+    , qform_code        :: Data.Word8       -- NIFTIXFORM code
+    , sform_code        :: Data.Word8       -- NIFTIXFORM code
+    , quatern_b         :: Float            -- Quaternion b param
+    , quatern_c         :: Float            -- Quaternion c param
+    , quatern_d         :: Float            -- Quaternion d param
+    , qoffset_x         :: Float            -- Quaternion x shift
+    , qoffset_y         :: Float            -- Quaternion y shift
+    , qoffset_z         :: Float            -- Quaternion z shift
+    , srow_x            :: Float            -- 1st row affine transform TODO: as array of floats
+    , srow_y            :: Float            -- 2nd row affine transform TODO: as array of floats
+    , srow_z            :: Float            -- 3rd row affine transform TODO: as array of floats
+    , intent_name       :: String           -- Name or meaning of data
+    , magic             :: String           -- MUST be "ni1\0" or "n+1\0"
+    }
 
-                          /**********************/    /**********************/     /***************/
-struct nifti_1_header {  /* NIFTI-1 usage      */    /*ANALYZE 7.5 field(s)*/     /* Byte offset */
-                        /**********************/    /**********************/     /***************/
+getNifti1HeaderLE :: Get Nifti1Header
+-- Nifti1 Little Endian
+getNifti1HeaderLE = do
+    sizeof_hdr <- getWord32le
+    skip 35 -- These 35 bytes of the header are unused
+    dim_info <- getWord8le
+    dim <- getWord16le
+    intent_p1 <- getFloatle
+    intent_p2 <- getFloatle
+    intent_p3 <- getFloatle
+    intent_code <- getWord8le
+    data_type <- getWord8le
+    bitpix <- getWord8le
+    slice_start <- getWord8le
+    pixdim <- getFloatle
+    vox_offset <- getFloatle
+    scl_slope <- getFloatle
+    scl_inter <- getFloatle
+    slice_end <- getWord8le
+    slice_code <- getWord8le
+    xyzt_units <- getWord8le
+    cal_max <- getFloatle
+    cal_min <- getFloatle
+    slice_duration <- getFloatle
+    toffset <- getFloatle
+    descrip <- getByteString 80 -- read exactly 80 bytes
+    aux_file <- getByteString 40 -- read exactly 40 bytes
+    qform_code <- getWord8le
+    sform_code <- getWord8le
+    quatern_b <- getFloatle
+    quatern_c <- getFloatle
+    quatern_d <- getFloatle
+    qoffset_x <- getFloatle
+    qoffset_y <- getFloatle
+    qoffset_z <- getFloatle
+    srow_x <- getFloatle
+    srow_y <- getFloatle
+    srow_z <- getFloatle
+    intent_name <- getByteString 16 -- read exactly 16 bytes
+    magic <- getByteString 4 -- read exactly 4 bytes
+    return $! Nifti1Header sizeof_hdr dim_info dim intent_p1 intent_p2 intent_p3 intent_code data_type bitpix slice_start pixdim vox_offset scl_slope scl_inter slice_end slice_code xyzt_units cal_max cal_min slice_duration toffset descrip aux_file qform_code sform_code quatern_b quatern_c quatern_d qoffset_x qoffset_y qoffset_z srow_x srow_y srow_z intent_name magic
 
+incrementalExample :: BL.ByteString -> [Trade]
+incrementalExample input0 = go decoder input0
+  where
+    decoder = runGetIncremental getTrade
+    go :: Decoder Trade -> BL.ByteString -> [Trade]
+    go (Done leftover _consumed trade) input =
+      trade : go decoder (BL.chunk leftover input)
+    go (Partial k) input                     =
+      go (k . takeHeadChunk $ input) (dropHeadChunk input)
+    go (Fail _leftover _consumed msg) _input =
+      error msg
 
- int   sizeof_hdr;    /*!< MUST be 348           */  /* int sizeof_hdr;      */   /*   0 */
+takeHeadChunk :: BL.ByteString -> Maybe BS.ByteString
+takeHeadChunk lbs =
+  case lbs of
+    (BL.Chunk bs _) -> Just bs
+    _ -> Nothing
 
- char  data_type[10]; /*!< ++UNUSED++            */  /* char data_type[10];  */   /*   4 */
+dropHeadChunk :: BL.ByteString -> BL.ByteString
+dropHeadChunk lbs =
+  case lbs of
+    (BL.Chunk _ lbs') -> lbs'
+    _ -> BL.Empty
 
- char  db_name[18];   /*!< ++UNUSED++            */  /* char db_name[18];    */   /*  14 */
-
- int   extents;       /*!< ++UNUSED++            */  /* int extents;         */   /*  32 */
-
- short session_error; /*!< ++UNUSED++            */  /* short session_error; */   /*  36 */
-
- char  regular;       /*!< ++UNUSED++            */  /* char regular;        */   /*  38 */
-
- char  dim_info;      /*!< MRI slice ordering.   */  /* char hkey_un0;       */   /*  39 */
-                                      /*--- was image_dimension substruct ---*/
- short dim[8];        /*!< Data array dimensions.*/  /* short dim[8];        */   /*  40 */
- float intent_p1;     /*!< 1st intent parameter. */  /* short unused8;       */   /*  56 */
-                                                     /* short unused9;       */
- float intent_p2;     /*!< 2nd intent parameter. */  /* short unused10;      */   /*  60 */
-                                                     /* short unused11;      */
- float intent_p3;     /*!< 3rd intent parameter. */  /* short unused12;      */   /*  64 */
-                                                     /* short unused13;      */
- short intent_code;   /*!< NIFTIINTENT code.     */  /* short unused14;      */   /*  68 */
- short datatype;      /*!< Defines data type!    */  /* short datatype;      */   /*  70 */
- short bitpix;        /*!< Number bits/voxel.    */  /* short bitpix;        */   /*  72 */
- short slice_start;   /*!< First slice index.    */  /* short dim_un0;       */   /*  74 */
- float pixdim[8];     /*!< Grid spacings.        */  /* float pixdim[8];     */   /*  76 */
- float vox_offset;    /*!< Offset into .nii file */  /* float vox_offset;    */   /* 108 */
- float scl_slope;     /*!< Data scaling: slope.  */  /* float funused1;      */   /* 112 */
- float scl_inter;     /*!< Data scaling: offset. */  /* float funused2;      */   /* 116 */
- short slice_end;     /*!< Last slice index.     */  /* float funused3;      */   /* 120 */
- char  slice_code;    /*!< Slice timing order.   */                               /* 122 */
- char  xyzt_units;    /*!< Units of pixdim[1..4] */                               /* 123 */
- float cal_max;       /*!< Max display intensity */  /* float cal_max;       */   /* 124 */
- float cal_min;       /*!< Min display intensity */  /* float cal_min;       */   /* 128 */
- float slice_duration;/*!< Time for 1 slice.     */  /* float compressed;    */   /* 132 */
- float toffset;       /*!< Time axis shift.      */  /* float verified;      */   /* 136 */
- int   glmax;         /*!< ++UNUSED++            */  /* int glmax;           */   /* 140 */
- int   glmin;         /*!< ++UNUSED++            */  /* int glmin;           */   /* 144 */
-
-                                         /*--- was data_history substruct ---*/
- char  descrip[80];   /*!< any text you like.    */  /* char descrip[80];    */   /* 148 */
- char  aux_file[24];  /*!< auxiliary filename.   */  /* char aux_file[24];   */   /* 228 */
- 
- short qform_code;    /*!< NIFTIXFORM code.      */  /*-- all ANALYZE 7.5 ---*/   /* 252 */
- short sform_code;    /*!< NIFTIXFORM code.      */  /*   fields below here  */   /* 254 */
-                                                     /*   are replaced       */
- float quatern_b;     /*!< Quaternion b param.    */                              /* 256 */
- float quatern_c;     /*!< Quaternion c param.    */                              /* 260 */
- float quatern_d;     /*!< Quaternion d param.    */                              /* 264 */
- float qoffset_x;     /*!< Quaternion x shift.    */                              /* 268 */
- float qoffset_y;     /*!< Quaternion y shift.    */                              /* 272 */
- float qoffset_z;     /*!< Quaternion z shift.    */                              /* 276 */
-
- float srow_x[4];     /*!< 1st row affine transform.   */                         /* 280 */
- float srow_y[4];     /*!< 2nd row affine transform.   */                         /* 296 */
- float srow_z[4];     /*!< 3rd row affine transform.   */                         /* 312 */
-
- char intent_name[16];/*!< name or meaning of data.  */                         /* 328 */
-
- char magic[4];       /*!< MUST be "ni1\0" or "n+1\0". */                         /* 344 */
-
-} ;                   /** 348 bytes total **/
-
-
--}
