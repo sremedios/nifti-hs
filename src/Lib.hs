47185a58d9a3cbb41450af1f05b354e6fb08d5ba
sremedios <sam.remedios@gmail.com>
2019-11-07 12:29:06 -0600

    simple load nifti file and copy based on two input prompts from user

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,24 @@
 module Lib
-    ( someFunc
+    ( copyNIFTI
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.IO
+
+getHandles :: IO (Handle, Handle)
+getHandles = do
+    -- TODO: make this parse cmd line args instead of prompting twice
+    in_filename <- getLine
+    out_filename <- getLine
+    in_handle <- openBinaryFile in_filename ReadMode
+    out_handle <- openBinaryFile out_filename WriteMode
+    return (in_handle, out_handle)
+
+copyNIFTI :: IO ()
+copyNIFTI = do
+    (inh, outh) <- getHandles
+    rawData <- hGetContents inh -- read content lazily
+    hPutStr outh rawData -- write content
+    -- TODO: do processing here
+    --close handles
+    hClose inh
+    hClose outh
